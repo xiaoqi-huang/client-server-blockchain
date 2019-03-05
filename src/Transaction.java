29e478c2ebf1348cb75842d0bae263770cbc7597
Xiaoqi Huang <xhua7314@uni.sydney.edu.au>
2019-03-05 21:29:36 +1100

    Implemented Transaction

--- src/Transaction.java
+++ src/Transaction.java
@@ -2,9 +2,20 @@ public class Transaction {
     private String sender;
     private String content;
 
-    // getters and setters
-    public void setSender(String sender) { this.sender = sender; }
-    public void setContent(String content) { this.content = content; }
+    // Setters
+    public void setSender(String sender) {
+        if (isValidSender(sender)) {
+            this.sender = sender;
+        }
+    }
+
+    public void setContent(String content) {
+        if (isValidContent((content))) {
+            this.content = content;
+        }
+    }
+
+    // Getters
     public String getSender() { return sender; }
     public String getContent() { return content; }
 
@@ -12,5 +23,12 @@ public class Transaction {
         return String.format("|%s|%70s|\n", sender, content);
     }
 
-    // implement helper functions here if you need any
+    // Helper functions
+    private boolean isValidSender(String sender) {
+        return sender != null && sender.matches("[a-z]{4}[0-9]{4}");
+    }
+
+    private boolean isValidContent(String content) {
+        return content.length() <= 70 && !content.contains("|");
+    }
 }
